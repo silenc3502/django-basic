59928fb09db14c0480ac9a6687cd57777ecf7e82
silenc3502 <gcccompil3r@gmail.com>
2023-02-05 20:59:51 +0900

    finish it

---
+++ second/blog/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+
+class PostDetail(DetailView):
+    model = Post
